4108789ab041621adad87e0403b47a605d74e343
chillhacker <gorohovdaniil19@gmail.com>
2026-03-21 17:30:55 +0300

    refactor(L1BlockOracle): minor improvements to align with rest of the contracts

--- contracts/interfaces/IL1BlockOracle.sol
+++ contracts/interfaces/IL1BlockOracle.sol
@@ -8,6 +8,9 @@ pragma solidity ^0.8.30;
  * @dev Provides a function to get the current L1 block number
  */
 interface IL1BlockOracle {
+    /// @notice Zero address not allowed for the submitter
+    error ZeroAddressNotAllowed(string field);
+
     /// @notice Submitted block number is not greater than the current value.
     error BlockNotMonotonic(uint256 current, uint256 proposed);
 
@@ -16,6 +19,18 @@ interface IL1BlockOracle {
 
     /// @dev Emitted when the submitter address is updated
     event SubmitterUpdated(address indexed oldSubmitter, address indexed newSubmitter);
+
+    /**
+     * @notice Updates the submitter address
+     * @param submitter The new submitter address
+     */
+    function setSubmitter(address submitter) external;
+
+    /**
+     * @notice Overrides the stored block number. Use only to correct a corrupted value.
+     * @dev Bypasses monotonicity check. Only callable by owner.
+     */
+    function setL1BlockNumber(uint256 blockNumber) external;
 
     /**
      * @notice Updates the current L1 block number
@@ -28,4 +43,10 @@ interface IL1BlockOracle {
      * @return The current L1 block number
      */
     function getL1BlockNumber() external view returns (uint256);
+
+    /**
+     * @notice Returns the submitter address
+     * @return The submitter address
+     */
+    function getSubmitter() external view returns (address);
 }
--- contracts/oracle/L1BlockOracle.sol
+++ contracts/oracle/L1BlockOracle.sol
@@ -8,46 +8,52 @@ import {IL1BlockOracle} from "../interfaces/IL1BlockOracle.sol";
  * @title L1BlockOracle
  * @author Fluent Labs
  * @notice Oracle contract for the L1 block number
- * @dev Provides a function to get the current L1 block number
+ * @dev Provides a function to get the current L1 block number and is used in the Fluent bridge
+ *      to check if a message is eligible for rollback.
  */
 contract L1BlockOracle is Ownable, IL1BlockOracle {
     /// @dev The current L1 block number
     uint256 internal _l1BlockNumber;
 
     /// @dev Hot key address authorized to submit block number updates.
-    address public submitter;
+    address internal _submitter;
 
-    constructor(address newSubmitter) Ownable(msg.sender) {
-        submitter = newSubmitter;
+    modifier onlySubmitter() {
+        require(msg.sender == _submitter, OwnableUnauthorizedAccount(msg.sender));
+        _;
+    }
+
+    constructor(address submitter) Ownable(msg.sender) {
+        _setSubmitter(submitter);
     }
 
     // ============ Submitter ============
 
     /// @inheritdoc IL1BlockOracle
-    function updateL1BlockNumber(uint256 blockNumber) external override {
-        if (msg.sender != submitter) revert OwnableUnauthorizedAccount(msg.sender);
-        if (blockNumber <= _l1BlockNumber) revert BlockNotMonotonic(_l1BlockNumber, blockNumber);
+    function updateL1BlockNumber(uint256 blockNumber) external override onlySubmitter {
+        require(blockNumber > _l1BlockNumber, BlockNotMonotonic(_l1BlockNumber, blockNumber));
+
         _l1BlockNumber = blockNumber;
         emit L1BlockNumberUpdated(blockNumber);
     }
 
     // ============ Owner ============
 
-    /**
-     * @notice Overrides the stored block number. Use only to correct a corrupted value.
-     * @dev Bypasses monotonicity check. Only callable by owner.
-     */
+    /// @inheritdoc IL1BlockOracle
     function setL1BlockNumber(uint256 blockNumber) external onlyOwner {
         _l1BlockNumber = blockNumber;
         emit L1BlockNumberUpdated(blockNumber);
     }
 
-    /**
-     * @notice Updates the submitter address.
-     */
-    function setSubmitter(address newSubmitter) external onlyOwner {
-        emit SubmitterUpdated(submitter, newSubmitter);
-        submitter = newSubmitter;
+    /// @inheritdoc IL1BlockOracle
+    function setSubmitter(address submitter) external override onlyOwner {
+        _setSubmitter(submitter);
+    }
+
+    function _setSubmitter(address submitter) internal {
+        if (submitter == address(0)) revert ZeroAddressNotAllowed("submitter");
+        emit SubmitterUpdated(_submitter, submitter);
+        _submitter = submitter;
     }
 
     // ============ Views ============
@@ -55,5 +61,10 @@ contract L1BlockOracle is Ownable, IL1BlockOracle {
     /// @inheritdoc IL1BlockOracle
     function getL1BlockNumber() external view override returns (uint256) {
         return _l1BlockNumber;
+    }
+
+    /// @inheritdoc IL1BlockOracle
+    function getSubmitter() external view override returns (address) {
+        return _submitter;
     }
 }
