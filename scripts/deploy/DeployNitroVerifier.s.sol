d5405c96155cbf331065856137e7a8a1aa5a4bd3
Dmitry Savonin <github@dmitry123.xyz>
2026-04-19 14:58:23 +0400

    feat: released new nitro verifier for mainnet

--- scripts/deploy/DeployNitroVerifier.s.sol
+++ scripts/deploy/DeployNitroVerifier.s.sol
@@ -20,11 +20,13 @@ contract DeployNitroVerifier is DeployBase {
         string memory json = _readConfig(vm.envOr("NETWORK", string("testnet/l1")));
         address sp1Verifier = vm.envOr("SP1_VERIFIER", json.readAddress(".rollup.sp1Verifier"));
         address adminRole = vm.envOr("ADMIN_ROLE", json.readAddress(".roles.admin"));
+        address enclaveAttesterRole = vm.envOr("ENCLAVE_ATTESTER_ROLE", json.readAddress(".roles.enclaveAttester"));
         string memory outputPath = vm.envOr("OUTPUT_PATH", string(""));
 
         console2.log("Deploying NitroVerifier");
         console2.log("  sp1Verifier:", sp1Verifier);
         console2.log("  admin:", adminRole);
+        console2.log("  enclaveAttester:", enclaveAttesterRole);
 
         vm.startBroadcast();
         address nitroVerifier = _deployNitroVerifier(sp1Verifier, adminRole);
@@ -32,8 +34,7 @@ contract DeployNitroVerifier is DeployBase {
         console2.log("NitroVerifier deployed:", nitroVerifier);
 
         bytes32 ENCLAVE_ATTESTER_ROLE = keccak256("ENCLAVE_ATTESTER_ROLE");
-        IAccessControl(nitroVerifier).grantRole(ENCLAVE_ATTESTER_ROLE, 0x4b6b03635aC1574E4966DD7A7f24d6FeB1bd3Be6);
-        IAccessControl(nitroVerifier).grantRole(ENCLAVE_ATTESTER_ROLE, 0x9ec3f0d76A6d3847d86374c791C6E170CAd9518D);
+        IAccessControl(nitroVerifier).grantRole(ENCLAVE_ATTESTER_ROLE, enclaveAttesterRole);
 
         vm.stopBroadcast();
 
